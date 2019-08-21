85bb531af7faf9d9f81a87b99f7de4e77f703ef7
davidsleeer <102831215@student.swin.edu.au>
2019-08-21 23:29:38 +1000

    number game

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,6 +7,45 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Random myRandom = new Random();
+            int s_n = myRandom.Next(0, 1000);
+
+            Console.WriteLine("what is the secret number?");
+            string y_o_b = Console.ReadLine();
+            int yob = int.Parse(y_o_b);
+            int i = 0;
+            while (yob != s_n)
+            {
+                if (i > 8)
+                {
+                    Console.WriteLine("Never seen an idiot like you! Count value 10! 10 chances wasted!!!");
+                    Console.WriteLine("The secret number is " + s_n);
+                    break;
+                }
+                if (yob > s_n)
+                {
+                    Console.WriteLine("Too big! Try again! Count value is " + (i + 1));
+                    y_o_b = Console.ReadLine();
+                    yob = int.Parse(y_o_b);
+                    i++;
+                }
+
+                if (yob < s_n)
+                {
+                    Console.WriteLine("Too samll! Try again! Count value is " + (i + 1));
+                    y_o_b = Console.ReadLine();
+                    yob = int.Parse(y_o_b);
+                    i++;
+                }
+
+
+                if (yob == s_n)
+                {
+                    Console.WriteLine(" Correct! You are a genius!");
+                }
+
+            }
         }
     }
 }
